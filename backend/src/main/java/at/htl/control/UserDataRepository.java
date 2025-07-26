d2a2d14e3f58e0658082bb0e7ff96ccf187ac6a6
KonstantinOeller <k.oeller@students.htl-leonding.ac.at>
2025-07-26 18:44:51 +0200

    added: UserDataRepository

---
+++ backend/src/main/java/at/htl/control/UserDataRepository.java
@@ -0,0 +1,7 @@
+package at.htl.control;
+
+import at.htl.model.UserData;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class UserDataRepository implements PanacheRepository<UserData> {
+}
