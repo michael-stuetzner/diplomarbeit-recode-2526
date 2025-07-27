377463e3329e74ae81438fa996898fb18e24bcfd
KonstantinOeller <k.oeller@students.htl-leonding.ac.at>
2025-07-27 17:24:15 +0200

    added: userData endpoint

--- backend/src/main/java/at/htl/boundary/UserDataResource.java
+++ backend/src/main/java/at/htl/boundary/UserDataResource.java
@@ -1,16 +1,25 @@
-package at.htl;
+package at.htl.boundary;
 
+import at.htl.control.UserDataRepository;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/UserData")
 public class UserDataResource {
 
+    @Inject
+    UserDataRepository userDataRepository;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        return Response.ok(
+                userDataRepository.findAll().list()
+        ).build();
     }
 }
